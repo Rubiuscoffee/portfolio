685f8f5132d6bbb5fbeb321981f49e71701a493a
rubiuscoffee <bsquintero_usistemas@intep.edu.co>
2025-10-22 18:42:32 -0500

    fix(slider): correct duplicate entries in slider data

--- src/components/Slider/sliderData.js
+++ src/components/Slider/sliderData.js
@@ -3,4 +3,8 @@ export const sliderData = [
   { title: 'Project 2', img: '/img/img2.avif', url: '/work' },
   { title: 'Project 3', img: '/img/img3.avif', url: '/work' },
   { title: 'Proyecto en proceso' },
+  { title: 'Project 1', img: '/img/img1.avif', url: '/work' },
+  { title: 'Project 2', img: '/img/img2.avif', url: '/work' },
+  { title: 'Project 3', img: '/img/img3.avif', url: '/work' },
+  { title: 'Proyecto en proceso' },
 ];
